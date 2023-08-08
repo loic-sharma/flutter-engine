67e522eb05dbce1c601c4805039177c34797fb6c
Loic Sharma <sharma.loic@gmail.com>
2023-08-08 15:05:31 -0700

    Refactor how semantic updates are created

---
+++ shell/platform/embedder/embedder_semantics_update.h
@@ -0,0 +1,72 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_SHELL_PLATFORM_EMBEDDER_EMBEDDER_SEMANTICS_UPDATE_H_
+#define FLUTTER_SHELL_PLATFORM_EMBEDDER_EMBEDDER_SEMANTICS_UPDATE_H_
+
+#include "flutter/shell/platform/embedder/embedder.h"
+#include "lib/ui/semantics/custom_accessibility_action.h"
+#include "lib/ui/semantics/semantics_node.h"
+
+namespace flutter {
+
+//------------------------------------------------------------------------------
+/// @brief      Line 1
+///             Line2
+///
+class EmbedderSemanticsUpdate {
+ public:
+  EmbedderSemanticsUpdate(
+    const SemanticsNodeUpdates& nodes,
+    const CustomAccessibilityActionUpdates& actions);
+
+  ~EmbedderSemanticsUpdate();
+
+  FlutterSemanticsUpdate* get() { return &update_; }
+
+ private:
+  FlutterSemanticsUpdate update_;
+  std::vector<FlutterSemanticsNode> nodes_;
+  std::vector<FlutterSemanticsCustomAction> actions_;
+
+  // Translates engine semantic nodes to embedder semantic nodes.
+  void AddNode(const SemanticsNode& node);
+
+  // Translates engine semantic custom actions to embedder semantic custom
+  // actions.
+  void AddAction(const CustomAccessibilityAction& action);
+
+  FML_DISALLOW_COPY_AND_ASSIGN(EmbedderSemanticsUpdate);
+};
+
+class EmbedderSemanticsUpdate2 {
+ public:
+  EmbedderSemanticsUpdate2(
+    const SemanticsNodeUpdates& nodes,
+    const CustomAccessibilityActionUpdates& actions);
+
+  ~EmbedderSemanticsUpdate2();
+
+  FlutterSemanticsUpdate2* get() { return &update_; }
+
+ private:
+  FlutterSemanticsUpdate2 update_;
+  std::vector<FlutterSemanticsNode2> nodes_;
+  std::vector<FlutterSemanticsNode2*> node_pointers_;
+  std::vector<FlutterSemanticsCustomAction2> actions_;
+  std::vector<FlutterSemanticsCustomAction2*> action_pointers_;
+
+  // Translates engine semantic nodes to embedder semantic nodes.
+  void AddNode(const SemanticsNode& node);
+
+  // Translates engine semantic custom actions to embedder semantic custom
+  // actions.
+  void AddAction(const CustomAccessibilityAction& action);
+
+  FML_DISALLOW_COPY_AND_ASSIGN(EmbedderSemanticsUpdate2);
+};
+
+}  // namespace flutter
+
+#endif  // FLUTTER_SHELL_PLATFORM_EMBEDDER_EMBEDDER_SEMANTICS_UPDATE_H_
